2e9d33ab295f5b2e849204de1aaba4b40c636554
Elaine <jwubv@connect.ust.hk>
2023-10-13 23:38:51 -0500

    feat: term filter

--- src/App.jsx
+++ src/App.jsx
@@ -36,24 +36,50 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // };
 
 
+const TermSelector = ({ selectedTerm, setSelectedTerm }) => {
+  const terms = ["Fall", "Winter", "Spring"];
+
+  return (
+    <div className="term-selector">
+      {terms.map(term => (
+        <button 
+          key={term} 
+          onClick={() => setSelectedTerm(term)}
+          className={selectedTerm === term ? "active" : ""}
+        >
+          {term}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+
 
 const App = () => {
   const [data, isLoading, error] = useJsonQuery(
     "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php"
   );
   
+  const [selectedTerm, setSelectedTerm] = useState("Fall");
+
+
   if (error) return <h1>Error loading Schedule data: {`${error}`}</h1>;
   if (isLoading) return <h1>Loading Schedule data...</h1>;
   if (!data) return <h1>No Schedule data found</h1>;
   
-  console.log(data)
+  // console.log(data)
   const schedule = data;
   return(
     <>
       <h1>{schedule.title}</h1>
+      <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+
       <div className = "courses-grid">
 
-      {Object.keys(schedule.courses).map(key => (
+      {Object.keys(schedule.courses)
+      .filter(key => schedule.courses[key].term === selectedTerm)
+      .map(key => (
         <>
         <div className="card m-1 p-2" key={key}>
         <h5 className="card-title" >{schedule.courses[key].term} CS {schedule.courses[key].number}</h5>
